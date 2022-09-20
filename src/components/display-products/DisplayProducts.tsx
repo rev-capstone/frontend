605401b4df050262adeec6add7e18e6eddc4a618
Khtran68 <khtran68@gmail.com>
2022-09-20 11:57:37 -0500

    Added Sort Dropdown

--- src/components/display-products/DisplayProducts.tsx
+++ src/components/display-products/DisplayProducts.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 import Product from '../../models/Product';
@@ -7,10 +7,23 @@ import Navbar from '../navbar/Narbar';
 import { ProductCard } from "./ProductCard";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding-right: 15%;
+  padding-left: 15%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const FilterContainer = styled.div`
+  
+  padding: 20px;
+  margin-top: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background-color: #f5fbfd;
 `;
 
 const Right = styled.div`
@@ -18,7 +31,12 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  margin-right: 10px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 export const DisplayProducts = () => {
@@ -26,6 +44,10 @@ export const DisplayProducts = () => {
   const [filter, setFilter] = useState("");
 
   const [products, setProducts] = useState<Product[]>([])
+
+  const filterChange = (event : any) => {
+    setFilter(event.target.value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,19 +61,43 @@ export const DisplayProducts = () => {
   return (
     <React.Fragment>
         <Navbar/>
-        <Right>
-          <TextField
-            id="search"
-            variant="outlined"
-            label="Search"
-            onChange={(e) => setFilter(e.target.value)}
-          />
-          </Right>
-        <Container id="product-container">
-        {products.filter((item) => item.name.toLocaleLowerCase().includes(filter)).map((item) => (
-            <ProductCard product={item} key={item.id} />
-        ))}
-        </Container>
+          <FilterContainer>
+            <Left>
+              <InputLabel id="sort">Sort</InputLabel>
+                <Select
+                  variant="standard"
+                  labelId="dropdown-sort"
+                  id="dropdown-sort"
+                  value={filter}
+                  onChange={filterChange}
+                  label="Sort"
+                >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                  <MenuItem value={20}>Price</MenuItem>
+                  <MenuItem value={30}>Quantity</MenuItem>
+                </Select>
+            </Left>
+            <Right>
+              <TextField
+                id="search"
+                variant="standard"
+                label="Search"
+                onChange={filterChange}
+                InputProps={{
+                  style: {
+                    paddingRight: '15%'
+                  },
+                }}
+              />
+            </Right>
+          </FilterContainer>
+          <Container id="product-container">
+            {products.filter((item) => item.name.toLocaleLowerCase().includes(filter)).map((item) => (
+                <ProductCard product={item} key={item.id} />
+            ))}
+          </Container>
     </React.Fragment>
     
   );
