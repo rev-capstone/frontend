6157bc2cad25d44cfb3f3d01e2332455158a3efd
JaDePat <jdp664@humboldt.edu>
2022-09-23 07:47:09 -0700

    added logout menu item

--- src/components/navbar/Narbar.tsx
+++ src/components/navbar/Narbar.tsx
@@ -7,6 +7,7 @@ import styled, { ThemeContext } from "styled-components";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { light, dark } from "../../context/theme.context";
+import { apiLogout } from "../../remote/e-commerce-api/authService";
 
 const Container = styled.div`
   height: 60px;
@@ -47,7 +48,7 @@ const Button = styled.div`
   top : 2px;
 `;
 let moon = <Brightness7Icon />;
-let sun = <Brightness4Icon/>;
+let sun = <Brightness4Icon />;
 let x = true;
 
 const Navbar = () => {
@@ -59,21 +60,29 @@ const Navbar = () => {
     x = !x;
   }
 
+  async function logout(){
+    let response = await apiLogout();
+    navigate('/');
+  }
+
   return (
     <Container>
       <Wrapper>
         <Left>
-        <Logo onClick={() => {navigate('/products')}}>Coder's Closet</Logo>
+          <Logo onClick={() => { navigate('/products') }}>Coder's Closet</Logo>
         </Left>
         <Right>
-          <MenuItem onClick={() => {navigate('/register')}}>REGISTER</MenuItem>
-          <MenuItem onClick={() => {navigate('/login')}}>SIGN IN</MenuItem>
-          <MenuItem onClick={() => {navigate('/cart')}}>
-            <Badge color="primary" overlap="rectangular">
-              <ShoppingCartOutlined />
-            </Badge>
-          </MenuItem>
-          <Button onClick={lightDark}>{x ? sun : moon}</Button>
+          {window.location.pathname != '/' && window.location.pathname != '/register' ?
+            <><MenuItem onClick={ logout }>LOGOUT</MenuItem>
+              <MenuItem onClick={() => { navigate('/cart'); }}>
+                <Badge color="primary" overlap="rectangular">
+                  <ShoppingCartOutlined />
+                </Badge>
+              </MenuItem><Button onClick={lightDark}>{x ? sun : moon}</Button></> :
+            <><MenuItem onClick={() => { navigate('/register') }}>REGISTER</MenuItem>
+              <MenuItem onClick={() => { navigate('/') }}>SIGN IN</MenuItem>
+              <Button onClick={lightDark}>{x ? sun : moon}</Button></>
+          }
         </Right>
       </Wrapper>
     </Container>
