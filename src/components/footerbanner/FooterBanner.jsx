7cd75fb3a3d26209a7c5aef694a1e3945bbf6aea
Remiiiiii <tuf09783@temple.edu>
2022-09-26 15:42:54 -0400

    design changes

--- src/components/footerbanner/FooterBanner.jsx
+++ src/components/footerbanner/FooterBanner.jsx
@@ -7,18 +7,18 @@ const FooterBanner = () => {
      <div className="footer-banner-container">
      <div className="banner-desc">
       <div className="left">
-        <p>
+      
         <h3>50% OFF</h3><br></br>
         <h1>Kev123</h1>
         <h1>200GB (2 x 100GB) DDR2</h1>
         <h1>DRAM 10000MHZ Memory Kit - Black</h1>
-        <img src={ram} alt="headphones" className="footer-banner-image"/>
-        </p>
-      </div>
-      <div className="right">
+        <div className="right">
         <h4>While Supplies Last!</h4>
-
       </div>
+        <img src={ram} alt="headphones" className="footer-banner-image"/>
+      
+      </div>
+     
      </div>
      </div>
  
